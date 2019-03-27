b5a0fa205318332e5e614a337394c8378bfe6dd4
Alexey Bauman <alxgenba@gmail.com>
2019-03-27 14:33:21 +0300

    add init_weights func

---
+++ src/utils/nn.py
@@ -0,0 +1,31 @@
+"""
+Utils for neural networks
+"""
+
+from torch import nn
+
+
+def init_weights_(m):
+    """
+    Initializes weights of m according to Xavier normal method.
+
+    :param m: module
+    :return:
+    """
+    if isinstance(m, nn.Conv2d):
+        nn.init.xavier_normal_(m.weight)
+        if m.bias is not None:
+            nn.init.xavier_normal_(m.bias)
+
+    elif isinstance(m, nn.BatchNorm2d):
+        nn.init.ones_(m.weight)
+        nn.init.zeros_(m.bias)
+
+    elif isinstance(m, nn.BatchNorm1d):
+        nn.init.ones_(m.weight)
+        nn.init.zeros_(m.bias)
+
+    elif isinstance(m, nn.Linear):
+        nn.init.xavier_normal_(m.weight)
+        if m.bias is not None:
+            nn.init.xavier_normal_(m.bias)
